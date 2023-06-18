0c166ef245d86ecee0e981aeb245429308002638
Fixfactory <5952846+fixfactory@users.noreply.github.com>
2023-06-18 11:03:38 -0700

    Removed pre-compiled header. Rename.

--- Source/Plugins/iRacing.Plugin/Main.cpp
+++ Source/Plugins/iRacing.Plugin/Main.cpp
@@ -1,5 +1,5 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#include <Windows.h>
 
 #include <string>
 #include <iostream>
