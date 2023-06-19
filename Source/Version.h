b5e859c26a8d9406baa4e748ee3e376aef5be364
Fixfactory <5952846+fixfactory@users.noreply.github.com>
2023-06-18 21:58:00 -0700

    Bumped version to 0.2.0

--- Source/Version.h
+++ Source/Version.h
@@ -7,7 +7,7 @@ namespace version
 {
     const std::string kName("Sli-Pro Super Pro");
     constexpr unsigned int kMajor = 0;
-    constexpr unsigned int kMinor = 1;
+    constexpr unsigned int kMinor = 2;
     constexpr unsigned int kRevision = 0;
 
     std::string getString()
