84f8bbf2fedfe7f18e38b64cdc3a4d1b25a257ed
Fixfactory <5952846+fixfactory@users.noreply.github.com>
2023-12-06 05:53:10 -0800

    Added copyright notice.

--- Source/SliProSuperPro/Timing.h
+++ Source/SliProSuperPro/Timing.h
@@ -1,3 +1,23 @@
+//
+// SliProSuperPro
+// A Shift Light Indicator controller
+// Copyright 2023 Fixfactory
+//
+// This file is part of SliProSuperPro.
+//
+// SliProSuperPro is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or any later version.
+//
+// SliProSuperPro is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with SliProSuperPro. If not, see <http://www.gnu.org/licenses/>.
+//
+
 #pragma once
 
 #include <chrono>
